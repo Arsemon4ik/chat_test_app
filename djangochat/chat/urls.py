9d6e8b9d864003f56ebf3deb04b2fa90160fef41
Arsemon4ik <76693557+Arsemon4ik@users.noreply.github.com>
2023-02-28 19:30:58 +0200

    Update urls.py

--- djangochat/chat/urls.py
+++ djangochat/chat/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     re_path(r'^thread_messages/(?P<thread_id>\w+)/$', views.get_messages_by_thread, name='thread_messages'),
     re_path(r'^delete_thread/(?P<thread_id>\w+)/$', views.delete_thread, name='delete_thread'),
     re_path(r'^view_message/(?P<message_id>\w+)/$', views.view_message, name='view_message'),
-    # якщо додати слеш в кінці то програма ламається
     re_path(r'^count_unread_messages', views.count_unread_messages, name='count_unread_messages'),
     path('', views.threads, name='threads'),
     re_path(r'^users', views.user_list, name='user_list')
